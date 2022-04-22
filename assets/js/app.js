11fd9bfbfec21088bf4e6c726356a8b6bcabaa97
AndreSoffiantini <andrea.soffiantini@hotmail.it>
2022-04-22 14:19:55 +0200

    Add milestone 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,10 @@ const app = new Vue({
     el: "#app",
 
     data: {
+
         tasks: [
             { text: 'Fare i compiti', done: false },
-            { text: 'Fare la spesa', done: false },
+            { text: 'Fare la spesa', done: true },
             { text: 'Fare il bucato', done: false },
         ]
     },
