9b57854160611b339596ad3b5ef1a716f05fd34d
AndreSoffiantini <andrea.soffiantini@hotmail.it>
2022-04-22 14:08:39 +0200

    Initial commit
    Add static layout

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+const app = new Vue({
+    el: "#app",
+
+    data: {
+        tasks: [
+            { text: 'Fare i compiti', done: false },
+            { text: 'Fare la spesa', done: false },
+            { text: 'Fare il bucato', done: false },
+        ]
+    },
+
+    methods: {
+
+    }
+})
